c870713a41587ab328815671d3132e3e61283fe7
Taha Elleuch <tahaeleuch@gmail.com>
2020-08-13 16:58:00 +0000

    fixes

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
 
 Base = declarative_base()
 
