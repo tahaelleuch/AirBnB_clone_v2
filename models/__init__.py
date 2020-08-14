568e4f3cd043af3ba16883db3d8cae22e4c805e2
fdetun <fouedads@gmail.com>
2020-08-14 21:56:47 +0100

    op

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This modulean object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
