0454c8fa026ad913e58a6ff6d8f1d008df5ebcc3
fdetun <fouedads@gmail.com>
2020-08-14 20:46:03 +0100

    fde

--- models/place.py
+++ models/place.py
@@ -26,18 +26,7 @@ class Place(BaseModel, Base):
         amenity_ids = []
         reviews = relationship("Review", backref="User", cascade="all, delete")
     else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
-
+        
         @property
         def reviews(self):
             from models.review import Review
