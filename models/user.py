0edfa714f75a5383cde0a4a800d7c729c455af61
Taha Elleuch <tahaeleuch@gmail.com>
2020-08-14 10:05:51 +0000

    first and last name can be null fixed

--- models/user.py
+++ models/user.py
@@ -10,15 +10,8 @@ from os import getenv
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
