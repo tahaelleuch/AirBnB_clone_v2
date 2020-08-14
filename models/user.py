660b7c27077d72000825ed87fb6f3b1abb48b633
fdetun <fouedads@gmail.com>
2020-08-14 12:11:51 +0100

    task8

--- models/user.py
+++ models/user.py
@@ -15,3 +15,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship("Place", backref="user")
