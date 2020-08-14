5939709ae5215bb76e3d33bcd2ed46c05c7ef10a
fdetun <fouedads@gmail.com>
2020-08-14 01:45:07 +0100

    user

--- models/user.py
+++ models/user.py
@@ -5,12 +5,20 @@ from models.base_model import BaseModel
 from models.base_model import Base
 import sqlalchemy
 from sqlalchemy import Column, String
+from os import getenv
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
