ac6780e81f0e49b7e8b0d749f77197d29d0d4dc1
Taha Elleuch <tahaeleuch@gmail.com>
2020-08-17 20:50:21 +0000

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,5 +8,5 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '29i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\tindex index.html\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '29i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
